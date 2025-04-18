374954291203228898d739adb797c4e1097accb8
jiny <jin.yang2@desaysv.com>
2025-04-18 11:42:43 +0800

    chatbot with memory only

--- chatbot.py
+++ chatbot.py
@@ -11,10 +11,6 @@ from langgraph.graph.message import add_messages
 from langgraph.prebuilt import ToolNode, tools_condition
 from IPython.display import Image, display
 
-# vLLM based llm.
-# from langchain_openai import ChatOpenAI
-# llm = ChatOpenAI()
-# llm.bind_tools()
 
 class State(TypedDict):
     messages: Annotated[list, add_messages]
@@ -22,29 +18,19 @@ class State(TypedDict):
 
 graph_builder = StateGraph(State)
 
-tool = TavilySearchResults(max_results=2)
-tools = [tool]
-# 使用 ChatOllama 并指定本地部署的模型名称
-llm = ChatOllama(model="llama3.2:1b")
-llm_with_tools = llm.bind_tools(tools)
 
+llm = ChatOllama(model="llama3.2:1b")
 
 def chatbot(state: State):
-    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+    return {"messages": [llm.invoke(state["messages"])]}
 
 
+# The first argument is the unique node name
+# The second argument is the function or object that will be called whenever
+# the node is used.
 graph_builder.add_node("chatbot", chatbot)
-
-tool_node = ToolNode(tools=[tool])
-graph_builder.add_node("tools", tool_node)
-
-graph_builder.add_conditional_edges(
-    "chatbot",
-    tools_condition,
-)
-# Any time a tool is called, we return to the chatbot to decide the next step
-graph_builder.add_edge("tools", "chatbot")
 graph_builder.set_entry_point("chatbot")
+graph_builder.set_finish_point("chatbot")
 memory = MemorySaver()
 graph = graph_builder.compile(checkpointer=memory)
 
