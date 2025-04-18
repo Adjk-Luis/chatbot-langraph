18ea00a828fe5861c7df8f9dd837fc881e33ef1e
jiny <jin.yang2@desaysv.com>
2025-04-18 16:56:30 +0800

    replace interrupt with input for exception catched

--- chatbot.py
+++ chatbot.py
@@ -13,6 +13,7 @@ from IPython.display import Image, display
 from langgraph.types import Command, interrupt
 from langchain_core.tools import tool, InjectedToolCallId
 from langchain_core.messages import ToolMessage
+from langgraph.graph import StateGraph, START, END
 
 # vLLM based llm.
 # from langchain_openai import ChatOpenAI
@@ -23,48 +24,55 @@ class State(TypedDict):
     name: str
     birthday: str
 
-graph_builder = StateGraph(State)
 
 @tool
-# Note that because we are generating a ToolMessage for a state update, we
-# generally require the ID of the corresponding tool call. We can use
-# LangChain's InjectedToolCallId to signal that this argument should not
-# be revealed to the model in the tool's schema.
 def human_assistance(
-    name: str, birthday: str, tool_call_id: Annotated[str, InjectedToolCallId]
-) -> str:
+    name: str, birthday: str, tool_call_id: Annotated[str, InjectedToolCallId]) -> str:
     """Request assistance from a human."""
-    human_response = interrupt(
-        {
-            "question": "Is this correct?",
+    print("========== human_assistance called ================")
+    try:
+        # human_response = interrupt(
+        #     {
+        #         "question": "Is this correct?",
+        #         "name": name,
+        #         "birthday": birthday,
+        #     },
+        # )
+        question = f"Is this correct? Name: {name}, Birthday: {birthday}. (yes/no): "
+        human_response = input(question).strip().lower()
+
+    except Exception as e:
+        print(f"An error occurred in interrupt function: {e}")
+        
+        # 可以根据具体情况返回一个错误信息给调用者
+        error_response = f"Error in interrupt: {e}"
+        state_update = {
             "name": name,
             "birthday": birthday,
-        },
-    )
-    print("==================", human_response, "==================")
-    # If the information is correct, update the state as-is.
-    if human_response.get("correct", "").lower().startswith("y"):
+            "messages": [ToolMessage(error_response, tool_call_id=tool_call_id)],
+        }
+        return Command(update=state_update)
+
+    if human_response == "yes" or human_response == "y":
         verified_name = name
         verified_birthday = birthday
         response = "Correct"
-    # Otherwise, receive information from the human reviewer.
     else:
-        verified_name = human_response.get("name", name)
-        verified_birthday = human_response.get("birthday", birthday)
+        verified_name = input("Please input correct name: ")
+        verified_birthday = input("Please input correct birthday: ")
         response = f"Made a correction: {human_response}"
 
-    # This time we explicitly update the state with a ToolMessage inside
-    # the tool.
     state_update = {
         "name": verified_name,
         "birthday": verified_birthday,
         "messages": [ToolMessage(response, tool_call_id=tool_call_id)],
     }
-    # We return a Command object in the tool to update our state.
     return Command(update=state_update)
+
 
 tool = TavilySearchResults(max_results=2)
 tools = [tool, human_assistance]
+
 # 使用 ChatOllama 并指定本地部署的模型名称
 llm = ChatOllama(model="qwen2.5:latest")
 llm_with_tools = llm.bind_tools(tools)
@@ -76,6 +84,7 @@ def chatbot(state: State):
     return {"messages": [message]}
 
 
+graph_builder = StateGraph(State)
 graph_builder.add_node("chatbot", chatbot)
 
 tool_node = ToolNode(tools=tools)
@@ -85,9 +94,9 @@ graph_builder.add_conditional_edges(
     "chatbot",
     tools_condition,
 )
-# Any time a tool is called, we return to the chatbot to decide the next step
 graph_builder.add_edge("tools", "chatbot")
-graph_builder.set_entry_point("chatbot")
+graph_builder.add_edge(START, "chatbot")
+
 memory = MemorySaver()
 graph = graph_builder.compile(checkpointer=memory)
 
